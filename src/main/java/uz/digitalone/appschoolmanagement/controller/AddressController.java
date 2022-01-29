e5dd56e95ae8899dd23b7cddeb18c476b8f3dae0
Doniyor <komiloff.doniyor2505@gmail.com>
2022-01-29 18:34:12 +0500

    added address crud

--- src/main/java/uz/digitalone/appschoolmanagement/controller/AddressController.java
+++ src/main/java/uz/digitalone/appschoolmanagement/controller/AddressController.java
@@ -1,9 +1,28 @@
 package uz.digitalone.appschoolmanagement.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import uz.digitalone.appschoolmanagement.entity.Address;
+import uz.digitalone.appschoolmanagement.service.AddressService;
+
 /**
  * Author: khamza@nightwell-logistics.com
  * Date: 1/27/2022
  * Time: 8:04 PM
  */
+
+@RestController
 public class AddressController {
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @PostMapping
+    public Address save(@RequestBody Address address) {
+        Address savedAddress = addressService.save(address);
+        return savedAddress;
+    }
 }
--- src/main/java/uz/digitalone/appschoolmanagement/service/AddressService.java
+++ src/main/java/uz/digitalone/appschoolmanagement/service/AddressService.java
@@ -1,9 +1,12 @@
 package uz.digitalone.appschoolmanagement.service;
 
+import uz.digitalone.appschoolmanagement.entity.Address;
+
 /**
  * Author: khamza@nightwell-logistics.com
  * Date: 1/27/2022
  * Time: 8:09 PM
  */
 public interface AddressService {
+    Address save(Address address);
 }
